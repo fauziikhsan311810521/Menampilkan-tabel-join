52a890f5e3d5299578056bece331a0fbc2f8cc8c
fauziikhsan311810521 <ikhsanfauzi063@gmail.com>
2021-07-14 15:37:57 +0700

    menampilkan table join

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php
+$host     = "localhost";
+$user     = "root";
+$password = "";
+$database = "fauziikhsan311810521";
+
+$hari_ini = date("Y-m-d");
+
+$conn  = new mysqli($host, $user, $password, $database);
+?>
